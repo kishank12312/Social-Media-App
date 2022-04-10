b577d192eafd3803fcac887300fe7d33e024d593
kishank12312 <kishankcr7@gmail.com>
2022-04-10 19:31:54 +0530

    Registered all relations on admin site

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -3,3 +3,10 @@ from .models import *
 # Register your models here.
 
 admin.site.register(UserDetails)
+admin.site.register(Posts)
+admin.site.register(PostLikes)
+admin.site.register(CommentLikes)
+admin.site.register(Comments)
+admin.site.register(PageFollowers)
+admin.site.register(Pages)
+admin.site.register(Friends)
