bb3bc001d1be41a1aa829a47727dc9de5c816f2d
kishank12312 <kishankcr7@gmail.com>
2022-04-25 16:57:08 +0530

    SignUp page styling completed

--- baseapp/forms.py
+++ baseapp/forms.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].help_text = 'Please Enter your email'
     class Meta:
         model = User 
         fields = ['username','email','password1', 'password2']
